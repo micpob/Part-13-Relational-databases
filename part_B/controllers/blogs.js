a784a1720abb5c306de27685fe661bfe27bea81d
micpob <bomboloapp@gmail.com>
2024-01-08 12:50:28 +0100

    Exercise 13.5

---
+++ part_B/controllers/blogs.js
@@ -0,0 +1,42 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    const result = await req.blog.destroy();
+    res.json(result)
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
