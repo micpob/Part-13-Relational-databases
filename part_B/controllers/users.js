0c39be28880eec0319d3db7e5c7ae785264e3dd7
micpob <bomboloapp@gmail.com>
2024-01-24 08:43:18 +0100

    Exercise 13.8

---
+++ part_B/controllers/users.js
@@ -0,0 +1,43 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch(error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id)
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.put('/:username', async (req, res, next) => {
+  const user = await User.findOne({ where: { username: req.params.username } })
+  if (user) {
+    try {
+      user.username = req.body.newUsername
+      await user.save()
+      res.json(user)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
